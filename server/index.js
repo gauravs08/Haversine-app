26c76b11f92d5a1d2e8d9ecbc2c2b047559239fb
Gaurav Sharma <gauravs08@users.noreply.github.com>
2019-06-01 23:20:15 +0300

    Updated Readme and added png for test

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
+const haversine = require('./haversine');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(pino);
+
+app.post(`/api/calculateDistance`, (req, res) => {
+
+  var start = {
+    latitude: req.body.lat1, //38.898556,
+    longitude: req.body.long1 //-77.037852
+  }
+  var end = {
+    latitude: req.body.lat2, //38.897147,
+    longitude: req.body.long2 //-77.043934
+  }
+  var resultCalc = haversine(start, end);
+  res.send(JSON.stringify({ calDist: `${resultCalc}` }));
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Point1: "",
+      Point2: "",
+      calculatedDistance: "",
+      errors: {}
+    };
+
+    this.handleChangeFrom = this.handleChangeFrom.bind(this);
+    this.handleChangeTo = this.handleChangeTo.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleValidation() {
+    let errors = {};
+    let formIsValid = true;
+
+    if (!this.state.Point1) {
+      formIsValid = false;
+      errors["Point1"] = "Cannot be empty";
+    }
+
+    if (!this.state.Point2) {
+      formIsValid = false;
+      errors["Point2"] = "Cannot be empty";
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  handleChangeFrom(event) {
+    this.setState({ Point1: event.target.value });
+  }
+
+  handleChangeTo(event) {
+    this.setState({ Point2: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.handleValidation()) {
+      const [latitute1, longitude1] = this.state.Point1.split(",");
+      const [latitute2, longitude2] = this.state.Point2.split(",");
+
+      let data = {
+        lat1: latitute1,
+        lat2: latitute2,
+        long1: longitude1,
+        long2: longitude2
+      };
+
+      fetch(`/api/calculateDistance`, {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" }
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            calculatedDistance: json.calDist
+          });
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App-home">
+        <header>
+          <form onSubmit={this.handleSubmit}>
+            <div className="App">
+              <p> Distance Between Two Points </p>
+            </div>
+            <div className="App-input">
+              <label>From: </label>
+              <input
+                id="Point1"
+                type="text"
+                value={this.state.Point1}
+                onChange={this.handleChangeFrom}
+                placeholder="latitude, longitude"
+                className="input"
+              />
+              <span className="error">{this.state.errors["Point1"]}</span>
+            </div>
+
+            <div className="App-input">
+              <label>To: </label>
+              <input
+                id="Point2"
+                type="text"
+                value={this.state.Point2}
+                onChange={this.handleChangeTo}
+                placeholder="latitude, longitude"
+                className="input"
+              />
+              <span className="error">{this.state.errors["Point2"]}</span>
+            </div>
+            <div className="App">
+              <button type="submit" className="App-button">
+                Calculate Distance
+              </button>
+            </div>
+            <div className="App">
+              <p>Calculated Distance is : {this.state.calculatedDistance}</p>
+            </div>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
